3b441636e1ff0279dbd6be811f608cc4d2059eaa
LIVIA CHANDRA <lchandra3@wisc.edu>
2024-04-03 14:11:28 -0500

    Remove unnecessary variables and codes from wav_conversion.py

--- backend_functionality/wav_conversion.py
+++ backend_functionality/wav_conversion.py
@@ -1,11 +1,11 @@
 ################################################################################
 # Filename: audio_conversion.py
-# Purpose:  Convert mp3 and m4a files into wav, and convert wav file to MIDI
-#           files.
+# Purpose:  Convert raw audio files to MIDI files
 # Author:   Livia Chandra
 #
 # Description:
-# [Detailed description of the contents and functionality of the file.]
+# The file helps in converting raw audio files in the format of mp3, wav, and 
+# m4a into MIDI files.
 #
 # Usage (Optional):
 # [Instructions or examples demonstrating how to use the code in this file.
@@ -20,59 +20,34 @@
 from pydub import AudioSegment as auseg
 import wave
 import aubio
-import numpy as np
-import mido
 from mido import MidiFile, MidiTrack, Message
-import midi_model as mm
 
 def convert_to_midi(audio):
     """
-    Function that convert all audio files into WAV files and return MIDI object.
+    Function that convert raw audio file into WAV file and return MIDI file.
 
     Args:
-        audio (String): Path to the audio file that in the format of mp3, m4a and wav
+        audio (String): Path to the audio file with the format of mp3, m4a and wav
 
     Returns:
-        midi: MIDI object that storing the audio file info in MIDI
+        midi: A MIDI file.
     """
 
     available_extension = ['m4a', 'mp3', 'wav']
 
-    # Get the name and the extension type of the audio file
+    # Get the name and the extension type of the input audio file
     file_name, extension = audio.split(".")
 
-    # Check the extension of the audio file
+    # Check the extension of the input audio file
     if extension not in available_extension:
         return None
 
-    # Convert audio files other to wav file
+    # Convert input audio file to wav file
     wav_file = auseg.from_file(audio, extension).export(
         file_name + ".wav", format = "wav"
     )
 
     # Convert wav files into MIDI file
-    midi_file_name = file_name + '.mid'
-    midi = wav_to_midi(wav_file, midi_file_name)
-
-    # Create midi object
-    midi_obj = mm.MIDI(0, 0, midi)
-
-    return midi_obj
-
-
-
-def wav_to_midi(wav_file, midi_file):
-    """
-    Helper function to help converting WAV files to MIDI files.
-
-    Args:
-        wav_file (String): Path to the wav audio file as input for conversion
-        midi_file (String): Desired output MIDI file name
-
-    Returns:
-        midi: MIDI object that storing the audio file info in MIDI
-    """
-
     # Read wav file for sample rate and number of frames
     with wave.open(wav_file, 'rb') as wav:
         params = wav.getparams()
@@ -87,7 +62,7 @@ def wav_to_midi(wav_file, midi_file):
     pitches.set_unit("midi")
 
     # Open the audio file
-    s = aubio.source(audio_file, frame_rate, hop_size)
+    s = aubio.source(wav_file, frame_rate, hop_size)
 
     # Create a new MIDI file and track
     midi = MidiFile()
@@ -100,7 +75,7 @@ def wav_to_midi(wav_file, midi_file):
         pitch = pitches(samples)[0]
 
         # Add MIDI notes only with a positive pitch value
-        if p > 0:  
+        if pitch > 0:  
             midi_note = int(round(pitch))
 
             # Write MIDI message and append to the MIDI track
@@ -113,7 +88,10 @@ def wav_to_midi(wav_file, midi_file):
         if curr_size < hop_size:
             break
 
-    # Save the MIDI file
-    midi.save(midi_file)
+    # Save MIDI file
+    midi_file_name = file_name + '.mid'
+    midi.save(midi_file_name)
 
+    # Return midi file
     return midi
+    
