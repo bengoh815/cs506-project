87abcaf5faaad3989ab22c0ea8700d07e45ae24c
Darren Seubert <dpseubert@wisc.edu>
2024-04-07 01:57:37 -0500

    Created class structure for db

--- server/app/models/db_orm.py
+++ server/app/models/db_orm.py
@@ -1,37 +1,36 @@
-from sqlalchemy import create_engine, Column, Integer, String 
-from sqlalchemy.ext.declarative import declarative_base 
-from sqlalchemy.orm import sessionmaker 
+from sqlalchemy import create_engine, Column, Integer, String, LargeBinary
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 from app.connection_string import MYSQL_CONNECTION_STRING
 
-# create an in-memory SQLite database 
-engine = create_engine(MYSQL_CONNECTION_STRING, echo=True) 
+Base = declarative_base()
 
-Base = declarative_base() 
 
-class User(Base): 
-	__tablename__ = 'users'
-	id = Column(Integer, primary_key=True) 
-	name = Column(String) 
-	fullname = Column(String) 
-	password = Column(String) 
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    email = Column(String)
 
-	def __repr__(self): 
-		return f"<User(name='{self.name}', 
-	fullname='{self.fullname}', password='{self.password}'>" 
+    def __repr__(self):
+        return f"<User(name='{self.name}', fullname='{self.fullname}', password='{self.password}'>"
 
-# create the users table 
-Base.metadata.create_all(engine) 
 
-# create a session to manage the connection to the database 
-Session = sessionmaker(bind=engine) 
-session = Session() 
+class Recordings(Base):
+    __tablename__ = "recordings"
+    recording_id = Column(Integer, primary_key=True)
+    name = Column(String)
+    user_id = Column(Integer)
 
-# add a new user to the database 
-user = User(name='john', fullname='John Doe', password='password') 
-session.add(user) 
-session.commit() 
+    def __repr__(self):
+        return f"<User(name='{self.name}', fullname='{self.fullname}', password='{self.password}'>"
 
-# query the users table 
-users = session.query(User).all() 
-print(users) 
-# Output: [<User(name='john', fullname='John Doe', password='password')>] 
+
+class MIDIs(Base):
+    __tablename__ = "midis"
+    midi_id = Column(Integer, primary_key=True)
+    recording_id = Column(Integer)
+    midi_data = Column(LargeBinary)
+
+    def __repr__(self):
+        return f"<User(name='{self.name}', fullname='{self.fullname}', password='{self.password}'>"
