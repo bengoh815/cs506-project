25ca2da00c10c1b07e50ec4cb906a07687832da2
Victor Nguyen <vnguyen45@wisc.edu>
2024-04-23 13:47:24 -0500

    remove unused import

--- client/src/components/upload/FileUpload.test.js
+++ client/src/components/upload/FileUpload.test.js
@@ -18,7 +18,6 @@
  ******************************************************************************/
 
 import { render, screen, cleanup } from "@testing-library/react";
-import renderer from "react-test-renderer";
 import FileUpload from "./FileUpload";
 
 describe("FileUpload component", () => {
