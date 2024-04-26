3850b736d268b39512a8bd5a2ef4be186b9f48d1
Benjamin <bzgoh@wisc.edu>
2024-04-25 20:52:56 -0500

    Code refactoring

--- client/src/components/upload/ConvertFileModal.jsx
+++ client/src/components/upload/ConvertFileModal.jsx
@@ -17,34 +17,7 @@
 import React from "react";
 import { useState } from "react";
 import { Button, Form, Modal, Spinner } from "react-bootstrap";
-
-function base64ToBlob(base64, mimeType) {
-  // Decode the base64 string to binary data
-  const byteCharacters = atob(base64);
-  const byteNumbers = new Array(byteCharacters.length);
-  for (let i = 0; i < byteCharacters.length; i++) {
-    byteNumbers[i] = byteCharacters.charCodeAt(i);
-  }
-  const byteArray = new Uint8Array(byteNumbers);
-
-  // Create a blob with the correct MIME type
-  return new Blob([byteArray], { type: mimeType });
-}
-
-function downloadMidi(dataBase64, filename) {
-  // Assume the MIME type is 'audio/midi'
-  const blob = base64ToBlob(dataBase64, "audio/midi");
-
-  // Create a link element, use it for downloading the blob, and remove it when done
-  const link = document.createElement("a");
-  link.href = window.URL.createObjectURL(blob);
-  link.download = filename; // Set the file name for the download
-
-  // Append the link, trigger the download, then remove the link
-  document.body.appendChild(link);
-  link.click();
-  document.body.removeChild(link);
-}
+import downloadMidi from "../../utils/downloadMidi";
 
 export default function ConvertFileModal(props) {
   // Whether a file is in the process of being converted
